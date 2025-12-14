2d215aa21c6d1db50b9711b820c4a87799808cc4
Bonchezz <bonchederrick@gmail.com>
2025-12-14 10:37:16 +0300

    render url

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+// Configuration for the frontend to connect to the backend
+window.AppConfig = {
+  API_BASE_URL: "https://smartclassroom-ji38.onrender.com/api",
+  SOCKET_URL: "https://smartclassroom-ji38.onrender.com"
+};
